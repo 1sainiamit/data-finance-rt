a487b5a9a2c0f84bb2854ddd5a04e4a0a150ac84
Amit Saini <96833654+1sainiamit@users.noreply.github.com>
2026-01-27 16:27:37 +0530

    test106

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <div className="flex text-white items-center h-24 max-w-[1240px] mx-auto px-4 justify-between">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">SAINI.</h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
+        <li className="p-4">Abhishek</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
