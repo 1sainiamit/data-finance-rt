317db4489a9080ad16516222e3fe8ee408824197
Dev Kumar <120044084+Dev-kumar-43@users.noreply.github.com>
2026-03-14 16:16:27 +0530

    Update Footer component structure and content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,27 @@ import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="max-w-[1240] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+      
       <div>
         <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+
         <p className="py-4">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
           perspiciatis cumque, reiciendis quo labore quidem quas eius tempore
           unde similique!
         </p>
+
+        {/* Images Section */}
+        <div className="flex gap-4 py-4">
+          <img src="https://elements.envato.com/graphics/noise+background" alt="img1" className="w-10 h-10 object-cover"/>
+          <img src="https://www.reddit.com/r/EnglishLearning/comments/1e73fon/is_there_a_word_for_when_theres_too_much_noise/" alt="img2" className="w-10 h-10 object-cover"/>
+          <img src="https://ocr.space/ocrapi" alt="img3" className="w-10 h-10 object-cover"/>
+          <img src="https://www.reddit.com/r/language/comments/18bku9a/what_is_written_here/" className="w-10 h-10 object-cover"/>
+          <img src="IMAGE_URL_5" alt="img5" className="w-10 h-10 object-cover"/>
+        </div>
+
+        {/* Social Icons */}
         <div className="flex md:w-[75%] my-6 justify-between">
           <FaFacebook size={30} />
           <FaGithub size={30} />
@@ -18,6 +31,7 @@ const Footer = () => {
           <FaTwitter size={30} />
         </div>
       </div>
+
       <div className="lg:col-span-2 flex justify-between mt-6">
         <div>
           <h6 className="font-medium text-gray-400">Solutions</h6>
@@ -28,8 +42,9 @@ const Footer = () => {
             <li className="py-2 text-sm">Insights</li>
           </ul>
         </div>
+
         <div>
-          <h6 className="font-medium text-gray-400">Solutions</h6>
+          <h6 className="font-medium text-gray-400">Resources</h6>
           <ul>
             <li className="py-2 text-sm">Pricing</li>
             <li className="py-2 text-sm">Documentation</li>
@@ -37,8 +52,9 @@ const Footer = () => {
             <li className="py-2 text-sm">API Status</li>
           </ul>
         </div>
+
         <div>
-          <h6 className="font-medium text-gray-400">Solutions</h6>
+          <h6 className="font-medium text-gray-400">Company</h6>
           <ul>
             <li className="py-2 text-sm">About</li>
             <li className="py-2 text-sm">Blog</li>
@@ -46,8 +62,9 @@ const Footer = () => {
             <li className="py-2 text-sm">Careers</li>
           </ul>
         </div>
+
         <div>
-          <h6 className="font-medium text-gray-400">Solutions</h6>
+          <h6 className="font-medium text-gray-400">Legal</h6>
           <ul>
             <li className="py-2 text-sm">Claim</li>
             <li className="py-2 text-sm">Policy</li>
@@ -55,6 +72,7 @@ const Footer = () => {
           </ul>
         </div>
       </div>
+
     </div>
   );
 };
