7600db7ecbfe5b7c1efe4f34aa8098c29dd6e998
Dev Kumar <120044084+Dev-kumar-43@users.noreply.github.com>
2026-03-19 10:14:12 +0530

    Update Navbar images to new sources

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       
       {/* Main Logo */}
       <img
-        src="https://imgs.search.brave.com/Aeb2tcIaXtRLRCMzNwQdfAEjXTznj6D78ngX1NM34S8/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi95b3Ut/aGF2ZS1iZWVuLWhh/Y2tlZC1oYWNrZXIt/YXR0YWNrLWluZm9y/bWF0aW9uLW1lc3Nh/Z2Utc2NyZWVuLTE4/NDU2MTU0NC5qcGc"
+        src="https://thumbs.dreamstime.com/b/you-have-been-hacked-wallpaper-you-have-been-hacked-wallpaper-d-rendering-200375866.jpg?w=992"
         alt="Logo"
         className="h-14 w-auto"
       />
@@ -22,16 +22,16 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <ul className="hidden md:flex items-center">
         <li className="p-4">
-          <img src="https://imgs.search.brave.com/dTB-qD19v6SBETcfwoON8vEKueFa_WdcvjadfdwE5jk/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9teWNv/YWNoaW5nLmluL3dw/LWNvbnRlbnQvdXBs/b2Fkcy9Qb2VtLTUu/d2VicA" alt="About" className="h-8 w-auto" />
+          <img src="https://static.vecteezy.com/system/resources/previews/015/286/164/non_2x/abstract-word-signal-or-are-warned-that-it-has-been-hacked-by-viruses-malware-or-hackers-on-a-red-digital-background-vector.jpg" alt="About" className="h-8 w-auto" />
         </li>
         <li className="p-4">
           <img src="https://imgs.search.brave.com/dB9biNLg7yGbKAsTrohlMZCT9yM7ceDdNnseOFp9qjg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9icmFp/bi10aGlua2luZy1j/cmVhdGl2ZS13b3Jk/LW92ZXItYWJzdHJh/Y3QtZ2VvbWV0cmlj/LXNoYXBlcy1iYWNr/Z3JvdW5kLXdlYi1i/YW5uZXItYnJhaW4t/dGhpbmtpbmctY3Jl/YXRpdmUtd29yZC1v/dmVyLWFic3RyYWN0/LTExMDE3NTIwOC5q/cGc" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://imgs.search.brave.com/lkjufuZXyitLwWq8rnYiX2TlzxNFrOHlMGT346_5Pg8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9pbmZv/Z3JhcGhpY3MtdmVj/dG9yLWJyYWluLWRl/c2lnbi1kaWFncmFt/LWJhbm5lci1saW5l/LXN0eWxlLXRlbXBs/YXRlLWNhbi1iZS11/c2VkLWJhbm5lcnMt/dGVjaG5vbG9neS1j/b25jZXB0LTUwMjAx/ODE5LmpwZw" alt="Resources" className="h-8 w-auto" />
+          <img src="https://m.media-amazon.com/images/I/51pcROeDxRL._UF1000,1000_QL80_.jpg" alt="Resources" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://imgs.search.brave.com/cs_WiO-D1geAImRSPI4XI4XdWHJpQyFCoFpvh7Yg40A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9lbGVt/ZW50cy1yZXNpemVk/LmVudmF0b3VzZXJj/b250ZW50LmNvbS9l/bGVtZW50cy1jb3Zl/ci1pbWFnZXMvMDgw/MDFmMjQtNzQ4My00/Nzg2LThhZDgtYThl/NmQyMDdiZGUzP3c9/NDMzJmNmX2ZpdD1z/Y2FsZS1kb3duJnE9/ODUmZm9ybWF0PWF1/dG8mcz04ZmY0OGMy/NGYwYjMzM2M4OTdk/M2UyZDhhYmViZTdh/MGJkY2M2OWIzNGQ5/Mjc5YjA2MmNmYjI2/YWY2ZDVhYjEx" alt="Blog" className="h-8 w-auto" />
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8LunubAeBsvstqVqRXmAskAWlpSXICDgn3g&s" alt="Blog" className="h-8 w-auto" />
         </li>
       </ul>
 
