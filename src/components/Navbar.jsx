92d04cdc98c28f74a884d2c26b6ae607b6b9c947
Dev Kumar <120044084+Dev-kumar-43@users.noreply.github.com>
2026-03-19 12:01:48 +0530

    Update Navbar component with new image sources

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,16 +22,17 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <ul className="hidden md:flex items-center">
         <li className="p-4">
-          <img src="" alt="About" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/wAQUKCmksVgYZ-RJkprHGp5ouEi-PqcEZZGZQvjRxDk/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2UyLzQz/LzIyL2UyNDMyMjc2/YzlhMDFlNGI4OTEz/ZTEzYzAxYjliMWY3/LmpwZw
+" alt="About" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/u3GIdrhccgBiFe0-tYmrqwAEqd8p71uo5dGAHHcwfJg/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTg0/NTQzMDQzMy92ZWN0/b3IvZnUtY2hpbmVz/ZS1uZXcteWVhci1n/cmVldGluZ3MtcG9w/dWxhci1uZXcteWVh/ci13b3Jkcy1kaXN0/aW5jdGl2ZS1oYW5k/d3JpdHRlbi1mb250/LWRlc2lnbi5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9VWx1/MUVua0tpVHpPVFBT/aU9sVUs1d0tSMW0x/U0d1ZFczdEl4UnU4/cnZoTT0" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="" alt="Resources" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/E_2-XFUxlfAtQCqq-3TEoas8VxnSE3v6I6S51DLAe9s/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMzgv/MTg4LzA1OC9zbWFs/bC9nb2xkZW4tYm9y/ZGVyLWNoaW5lc2Ut/bmV3LXllYXItd29y/ZGluZy1oYXBweS1u/ZXcteWVhci1mcmVl/LXZlY3Rvci5qcGc" alt="Resources" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="" alt="Blog" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/l8haXjUoVSY9CnRfWOKvlLOwWRpOy6gHL9iMl-EptPE/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzhiL2Y3/LzczLzhiZjc3MzIx/NmMzZGM2MTM4OWQz/MDNiNDNkNDJmYTRm/LmpwZw" alt="Blog" className="h-8 w-auto" />
         </li>
       </ul>
 
