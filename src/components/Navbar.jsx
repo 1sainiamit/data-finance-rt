d02f238de640316bfec4207c28ca45a8c5ec9b02
Dev Kumar <120044084+Dev-kumar-43@users.noreply.github.com>
2026-03-24 23:08:57 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       
       {/* Main Logo */}
       <img
-        src="https://imgs.search.brave.com/UwZENeDAFnxRo_0ciX239XtMa_03f6JZgHhy7MrUkAI/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM5/MDM1NjQzNi9waG90/by9mbGFnLW9mLXBh/a2lzdGFuLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz11eXky/UndKWWtxY08zUW14/S0VYSmtOci1LQWFG/cFJDWVFoeEZNVmNV/a1VJPQ"
+        src="https://imgs.search.brave.com/lvl3Bk30Py0pkkUpxGLBB-dLYaKLeFdfwY3PHo-CBVU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzU0LzQyLzI5/LzM2MF9GXzI1NDQy/Mjk5M19EM2NFTTZW/TkdiSXRRbURRaU1h/Mk5mMmc3NkY2S0Rs/Ty5qcGc"
         alt="Logo"
         className="h-14 w-auto"
       />
@@ -22,7 +22,7 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <ul className="hidden md:flex items-center">
         <li className="p-4">
-          <img src="https://imgs.search.brave.com/VoeKqDz7Lb5tuwnoUdF4y1JT3LyHIohH-nx_PzEp5FQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/c2h1dHRlcnN0b2Nr/LmNvbS9pbWFnZS12/ZWN0b3IveW91LWhh/dmUtYmVlbi1oYWNr/ZWQtZ2xpdGNoLTI2/MG53LTExNzMyMzA0/NzMuanBn" alt="About" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/7YLQrz2KndoWzrLDKYEgOZUhI1BB6dJM7XdQZ9C9H74/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jeWJl/cnNlY3VyaXR5dmVu/dHVyZXMuY29tL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDIxLzAz/L3N5c3RlbS1oYWNr/ZWQuanBn" alt="About" className="h-8 w-auto" />
         </li>
         <li className="p-4">
           <img src="https://imgs.search.brave.com/Aeb2tcIaXtRLRCMzNwQdfAEjXTznj6D78ngX1NM34S8/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi95b3Ut/aGF2ZS1iZWVuLWhh/Y2tlZC1oYWNrZXIt/YXR0YWNrLWluZm9y/bWF0aW9uLW1lc3Nh/Z2Utc2NyZWVuLTE4/NDU2MTU0NC5qcGc" className="h-8 w-auto" />
