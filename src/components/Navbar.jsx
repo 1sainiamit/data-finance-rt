ea1247b0f87a97f7f69b1a422a964ea54ee7a772
Dev Kumar <120044084+Dev-kumar-43@users.noreply.github.com>
2026-03-25 21:44:15 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       
       {/* Main Logo */}
       <img
-        src="https://imgs.search.brave.com/n4rHlK5l_WNreGKelOakCsIqFFUpwSV1-NWh-taNaKY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9wbHVz/LnVuc3BsYXNoLmNv/bS9wcmVtaXVtX3Bo/b3RvLTE2NzU4NzU0/ODc2ODktYzhiM2Rj/YjRiMjc5P2ZtPWpw/ZyZxPTYwJnc9MzAw/MCZhdXRvPWZvcm1h/dCZmaXQ9Y3JvcCZp/eGxpYj1yYi00LjEu/MCZpeGlkPU0zd3hN/akEzZkRCOE1IeHpa/V0Z5WTJoOE5YeDhj/R0ZyYVhOMFlXNGxN/akJtYkdGbmZHVnVm/REI4ZkRCOGZId3c"
+        src="https://imgs.search.brave.com/2BBOimnTP2_x1YCFlowLQhGC1-lKinlmtJXvyhKmPcQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jOC5h/bGFteS5jb20vY29t/cC9NNENHUEgvaGFj/a2VkLWN5YmVyLXNl/Y3VyaXR5LWFuZC1v/bmxpbmUtZnJhdWQt/Y29uY2VwdC1tb2Jp/bGUtaGFja2VyLWFu/ZC1jcmltaW5hbC1N/NENHUEguanBn"
         alt="Logo"
         className="h-14 w-auto"
       />
@@ -22,16 +22,16 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <ul className="hidden md:flex items-center">
         <li className="p-4">
-          <img src="https://imgs.search.brave.com/25v6rpIIySksRm0HH1V9jdwWR-ualgdNGv-6vmp9gWM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9oYWNr/ZWQtdGFibGV0LWNv/bXB1dGVyLWNvbXB1/dGVycy13aWxsLWJl/Y29tZS1ncmVhdC10/YXJnZXQtdG8taW50/ZXJuZXQtaGFja2Vy/cy1jcmltaW5hbHMt/MzgyOTg1NDUuanBn" alt="About" className="h-8 w-auto" />
+          <img src="https://t4.ftcdn.net/jpg/17/32/25/51/360_F_1732255115_V9H5mDKLIgARIdDfMQh2htqUAZiimX2M.jpg" alt="About" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://imgs.search.brave.com/Aeb2tcIaXtRLRCMzNwQdfAEjXTznj6D78ngX1NM34S8/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi95b3Ut/aGF2ZS1iZWVuLWhh/Y2tlZC1oYWNrZXIt/YXR0YWNrLWluZm9y/bWF0aW9uLW1lc3Nh/Z2Utc2NyZWVuLTE4/NDU2MTU0NC5qcGc" className="h-8 w-auto" />
+          <img src="https://imagesvs.oneindia.com/img/2014/09/14-hindi-600.jpg" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://www.optilingo.com/wp-content/uploads/2020/09/690232_OptiLingoHindiAlphabet_v2_Opt2_041520-1-1002x1536-1.png" alt="Resources" className="h-8 w-auto" />
+          <img src="https://media.istockphoto.com/id/1437997089/vector/indian-languages-word-cloud-vector-illustration.jpg?s=612x612&w=0&k=20&c=JWzmnXCUJj925PmLNc-1MIYhN19-sf8eAO7Onbl6paA=" alt="Resources" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://i.pinimg.com/236x/7c/79/3b/7c793bbe17e05d18fae1fade80471d75.jpg" alt="Blog" className="h-8 w-auto" />
+          <img src="https://www.optilingo.com/wp-content/uploads/2020/09/690232_OptiLingoHindiAlphabet_v2_Opt2_041520-1-1002x1536-1.png" alt="Blog" className="h-8 w-auto" />
         </li>
       </ul>
 
