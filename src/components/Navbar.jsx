87658825c242973e8cb61994c10b758f1ad9fde4
Dev Kumar <120044084+Dev-kumar-43@users.noreply.github.com>
2026-03-27 22:26:59 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       
       {/* Main Logo */}
       <img
-        src="https://images.indianexpress.com/2019/09/hindi-diwas-gettyimages-616123470.jpg"
+        src="https://imgs.search.brave.com/VVoxwz9ESCMj3T8vtzRnSKGcgXnPwqclv_rbpxkNWzU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzhiLzNh/LzhkLzhiM2E4ZGRh/OWMxNmM2MjBmNjIw/NjllNzUzYzFhZDQ3/LmpwZw"
         alt="Logo"
         className="h-14 w-auto"
       />
@@ -22,16 +22,16 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <ul className="hidden md:flex items-center">
         <li className="p-4">
-          <img src="https://imgs.search.brave.com/P3I3XfYluahpKZ2r-wydk8ERghD7I4vA5mFL9zTT6DQ/rs:fit:0:180:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMud29uZGVyc2hh/cmUuY29tL3BkZmVs/ZW1lbnQvb2NyL3Vy/ZHUtc2FtcGxlLmpw/Zw" alt="About" className="h-8 w-auto" />
+          <img src="https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2019/09/hindi-diwas-1568428091.jpg" alt="About" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://imgs.search.brave.com/VVoxwz9ESCMj3T8vtzRnSKGcgXnPwqclv_rbpxkNWzU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzhiLzNh/LzhkLzhiM2E4ZGRh/OWMxNmM2MjBmNjIw/NjllNzUzYzFhZDQ3/LmpwZw" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/P3I3XfYluahpKZ2r-wydk8ERghD7I4vA5mFL9zTT6DQ/rs:fit:0:180:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMud29uZGVyc2hh/cmUuY29tL3BkZmVs/ZW1lbnQvb2NyL3Vy/ZHUtc2FtcGxlLmpw/Zw" className="h-8 w-auto" />
         </li>
         <li className="p-4">
           <img src="https://imgs.search.brave.com/ffTrPN6zjUxIR1YaxeTKapA9Aw6T2u1-tBnOlguaUcA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzBiL2Yw/L2MxLzBiZjBjMTNm/NTBhZmZiNmY5Yzg2/NzIxY2E2NmVmMjNm/LmpwZw" alt="Resources" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2019/09/hindi-diwas-1568428091.jpg" alt="Blog" className="h-8 w-auto" />
+          <img src="https://images.indianexpress.com/2019/09/hindi-diwas-gettyimages-616123470.jpg" alt="Blog" className="h-8 w-auto" />
         </li>
       </ul>
 
