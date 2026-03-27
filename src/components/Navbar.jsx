058ea2d1b8407aadf81b3a64cc405211bd70cb9d
Dev Kumar <120044084+Dev-kumar-43@users.noreply.github.com>
2026-03-27 21:43:38 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       
       {/* Main Logo */}
       <img
-        src="https://static.vecteezy.com/system/resources/previews/015/286/164/large_2x/abstract-word-signal-or-are-warned-that-it-has-been-hacked-by-viruses-malware-or-hackers-on-a-red-digital-background-vector.jpg"
+        src="https://imgs.search.brave.com/Bd9q-Ntm68gaS_1G7E7GyVEUrV4oIoz9qlUbIEu28Gw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9lbGVt/ZW50cy1yZXNpemVk/LmVudmF0b3VzZXJj/b250ZW50LmNvbS9l/bGVtZW50cy1jb3Zl/ci1pbWFnZXMvMzdk/NWI3OTctN2U1OS00/YjljLWE5OWMtYmVk/ZmNlZTQ4MmQ2P3c9/NDMzJmNmX2ZpdD1z/Y2FsZS1kb3duJnE9/ODUmZm9ybWF0PWF1/dG8mcz1kNzRkNTY3/YzJiY2Y2ZTlmY2U0/Y2EyMWRlNWU4ZDZi/OTRiYzMxZDg3YzJm/NzY5MTJhMDE0OWRj/NjQ1NWFmYzY1"
         alt="Logo"
         className="h-14 w-auto"
       />
@@ -22,16 +22,16 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <ul className="hidden md:flex items-center">
         <li className="p-4">
-          <img src="https://media.istockphoto.com/id/1437997089/vector/indian-languages-word-cloud-vector-illustration.jpg?s=612x612&w=0&k=20&c=JWzmnXCUJj925PmLNc-1MIYhN19-sf8eAO7Onbl6paA=" alt="About" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/P3I3XfYluahpKZ2r-wydk8ERghD7I4vA5mFL9zTT6DQ/rs:fit:0:180:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMud29uZGVyc2hh/cmUuY29tL3BkZmVs/ZW1lbnQvb2NyL3Vy/ZHUtc2FtcGxlLmpw/Zw" alt="About" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://imagesvs.oneindia.com/img/2014/09/14-hindi-600.jpg" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/VozIglkiOeKWQPK1Lqg54Q_0Ll_FlOP4N6kyDDcOhFI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1QlpEWXhPVE01/Wm1NdFptTTNOaTAw/WVdObExXSTNZamt0/TmpSbVpXSTFabUk1/WVRKa1hrRXlYa0Zx/Y0djQC5qcGc" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://t4.ftcdn.net/jpg/17/32/25/51/360_F_1732255115_V9H5mDKLIgARIdDfMQh2htqUAZiimX2M.jpg" alt="Resources" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/TTgRmdJjfnlTc8kqeHv9SM5iUxmJxphU-wK4EUMRevY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1Qk9HVTFNakF6/WWpJdE5URmlZeTAw/WkRRNExXRTVORFl0/TVdVNFlUQTBORFJt/TkdaalhrRXlYa0Zx/Y0djQC5qcGc" alt="Resources" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://imagesvs.oneindia.com/img/2014/09/14-hindi-600.jpg" alt="Blog" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/uUBQZ-ZSrD1SEbsiDkC7XdXzXD0eyeT054Vp15sNhJA/rs:fit:0:180:1:0/g:ce/aHR0cHM6Ly9pLnNz/dGF0aWMubmV0L01o/ekxBLmpwZw" alt="Blog" className="h-8 w-auto" />
         </li>
       </ul>
 
