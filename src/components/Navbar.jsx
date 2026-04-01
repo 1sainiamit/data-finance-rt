e888719dc213235b93aa64cc0cad0d16434d955a
Dev Kumar <120044084+Dev-kumar-43@users.noreply.github.com>
2026-04-01 22:33:24 +0530

    Update Navbar component images and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       
       {/* Main Logo */}
       <img
-        src="https://imgs.search.brave.com/zI8mk4EIbKZkG0V4FOPWxccxaKpfZmOWS8YqpAzrKUE/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5jbm4uY29tL2Fw/aS92MS9pbWFnZXMv/c3RlbGxhci9wcm9k/LzE1MDIxODE1MzYx/MS0wOS1jbm5waG90/b3MtaXdvLWppbWEt/cmVzdHJpY3RlZC5q/cGc_cT13XzIwMDAs/aF8xMTI1LHhfMCx5/XzAsY19maWxsL2hf/NDQ3"
+        src="https://imgs.search.brave.com/UP5qghy5SmuXK8i9QndV6zSdI7dp54zGKVIEUWuwAhw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/c2h1dHRlcnN0b2Nr/LmNvbS9pbWFnZS12/ZWN0b3IvaGluZGkt/YWxwaGFiZXRzLXNj/cmlwdC1jYWxsaWdy/YXBoeS0yNjBudy0x/MDM3MDY0ODk1Lmpw/Zw"
         alt="Logo"
         className="h-14 w-auto"
       />
@@ -22,16 +22,16 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <ul className="hidden md:flex items-center">
         <li className="p-4">
-          <img src="https://cdn.3dnames.co/previews/cd47/1000x1000/h/cd47hacker3dxx.webp" alt="About" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/YassGYTZQUgl8oJiPsxyvXxhaQDbhlhupStfS2oMi8Y/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/MzY2OTAxNzk0ODMt/N2EwZjFkMGVmYzA1/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZpeGxp/Yj1yYi00LjEuMCZp/eGlkPU0zd3hNakEz/ZkRCOE1IeHpaV0Z5/WTJoOE1UQjhmR2hw/Ym1ScEpUSXdiR0Z1/WjNWaFoyVjhaVzU4/TUh4OE1IeDhmREE9" alt="About" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://hewlett.org/wp-content/uploads/2019/07/cyber-hacker-hoodie-photo.jpg" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/FtJjG8FN6p_gm1rLdV7UGWLullNOaWYxiFGJmPIQeCI/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTI5/NjAzMTU2NC9waG90/by9yb2FkLXNpZ24t/d2l0aC1lbWVyZ2Vu/Y3ktaGVscGxpbmUt/bnVtYmVycy1uZXct/ZGVsaGktaW5kaWEu/anBnP3M9NjEyeDYx/MiZ3PTAmaz0yMCZj/PUV5ekFOc2l4dnQt/amlxY29YMGZwLUZ6/UXo5WUh1M0VLTlR1/MFJNbmVNUG89" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://cdn.vectorstock.com/i/1000v/61/72/hacker-wordmark-symbol-set-vector-35916172.jpg" alt="Resources" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/a54Hlsz6IRgzMztoM2UIfQc5hzoOR8wQLl0kVZhfXlc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMjEv/NjM5LzA0MC9zbWFs/bC9zYW50LWthYmly/LWRvaGUtaW4taGlu/ZGktdGV4dC1tZWFu/aW5nLWRvbnQtYXNr/LXRoZS1jYXN0LW9m/LWh1bWFucy1qdXN0/LWFzay1mb3Ita25v/d2xlZGdlLWRvLXZh/bHVlLW9mLXRoZS1z/d29yZC1kb250LWNv/bnNpZGVyLW9mLWl0/cy1wb2NrZXQtdmVj/dG9yLmpwZw" alt="Resources" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://avatars.githubusercontent.com/u/2513332?s=280&v=4" alt="Blog" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/_KU9I13ozDnjzP_KpCiiWEzXyA8P5D1VDqgn8XVYJRg/rs:fit:0:180:1:0/g:ce/aHR0cHM6Ly9iLnRo/dW1icy5yZWRkaXRt/ZWRpYS5jb20vaDNV/Mm9EbEpVb0dLRTBS/Rl83aHgtOHN6VkN4/bXFCWDc1MnNBMi14/M2pjQS5qcGc" alt="Blog" className="h-8 w-auto" />
         </li>
       </ul>
 
