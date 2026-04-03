f5d0807dbf1f8ecc39d319d1c1939a68bd65dec5
Dev Kumar <120044084+Dev-kumar-43@users.noreply.github.com>
2026-04-03 15:51:09 +0530

    Modify Hero component text content
    Updated text content in Hero component for clarity.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="text-white">
       <div className="max-w-[800px] mt-[96px] w-full mx-auto text-center flex flex-col justify-center">
         <p className="text-[#00df9a] font-bold p-2">
-          GROWING WITH DATA ANALYTICS
+          GROWING WITH DATA ANALYTICS hacked the pwned
         </p>
         <h1 className="md:text-7xl sm:text-6xl font-bold md:py-6">
           Grow with data.
@@ -24,7 +24,7 @@ const Hero = () => {
           />
         </div>
         <p className="md:text-2xl text-xl font-bold text-gray-500">
-          Monitor your data analytics to increase revenue for BTB, BTC, SASS
+          Monitor your data analytics to increase revenue for BTB, BTC, SASS,jalksdjflkajsdlfjalsdfna hacked aldjflkasjdfl qwehrwlehrlqwejrlkqjwelrjwerl;kjqwelkrj
           platform.
         </p>
         <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
