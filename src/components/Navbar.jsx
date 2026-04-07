f4cfbc3e0b4979ec7264b05c4b4c560b9a6ecd9a
Dev Kumar <120044084+Dev-kumar-43@users.noreply.github.com>
2026-04-07 21:55:13 +0530

    Change Blog image and fix Navbar menu items
    Updated image source for Blog and modified menu items.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
           <img src="https://imgs.search.brave.com/j1ccJ1ONjDGKoXdY-9tbisi2Bnct1wGsYObZnTVOQis/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzM4Lzgw/LzM2LzM4ODAzNjNm/ZTE3NzgzZGYyZmQy/YzczODM2MzYyNDQ2/LmpwZw" alt="Resources" className="h-8 w-auto" />
         </li>
         <li className="p-4">
-          <img src="https://imgs.search.brave.com/eLgAlemH1qheT3WKtn5CDaiL3cpGN8AAaJ9mNrX0Eo8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9zaG93/aW5nLWhhY2tlZC1v/dmVybGF5LWN1dHRp/bmctYWNyb3NzLXNl/cnZlci1haXNsZS1y/YWNrcy1yZWQtY2ly/Y3VpdC10cmFjZXMt/YmluYXJ5LWRpZ2l0/cy1jeWJlcnNlY3Vy/aXR5LWRpZ2l0YWwt/Mzg4NjY5OTI4Lmpw/Zw" alt="Blog" className="h-8 w-auto" />
+          <img src="https://imgs.search.brave.com/F_5Cxfrg89TAIVImf6q5aVPMw4pZHtAfJgJmdDwVFRs/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzE1LzM1LzAwLzIy/LzM2MF9GXzE1MzUw/MDIyMzFfY2dNYXhh/Y2Y5MTR4ck55UHFl/TnVxMEhGR3pQeTVE/MnAuanBn" alt="Blog" className="h-8 w-auto" />
         </li>
       </ul>
 
@@ -54,7 +54,7 @@ const Navbar = () => {
           className="h-14 m-4"
         />
 
-       <ul className="uppercase p-4"> <li className="p-4 border-b border-b-gray-600">Aboutasdljhfhalsdjf</li> <li className="p-4 border-b border-b-gray-600">Companyla;jshd;flhjasldjhf</li> <li className="p-4 border-b border-b-gray-600">Resources</li> <li className="p-4 border-b border-b-gray-600">Pakistan</li> <li className="p-4">Contact</li> </ul>
+       <ul className="uppercase p-4"> <li className="p-4 border-b border-b-gray-600">Aboutasdljhasdfasdfasdfasdfasfhalsdjf</li> <li className="p-4 border-b border-b-gray-600">Coampanyla;jshd;flhjasldjhf</li> <li className="p-4 border-b border-b-gray-600">Resources</li> <li className="p-4 border-b border-b-gray-600">Pakistan</li> <li className="p-4">Contact</li> </ul>
       </div>
     </nav>
   );
